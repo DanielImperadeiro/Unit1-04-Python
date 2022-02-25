1d8818b0bd5c62b8623975c19cf580c7b0caf730
Ubuntu <ubuntu@ip-172-31-15-252.ec2.internal>
2022-02-25 14:13:34 +0000

    jdwkcwkc

---
+++ area_per_rect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created By: Daniel
+# Date: Feb. 22, 2022
+# This program displays the area and perimeter of a rectangle.
+
+
+def main():
+    print("For a rectangle that has the dimensions")
+    print("length 5 cm and width 7 cm:\n")
+    print("area = {}".format(5 * 7), "cm^2")
+    print("perimeter = {}".format(2 * (5 + 7)), "cm")
+
+
+if __name__ == "__main__":
+    main()
