849882b12e7d8b4714ba824cc3c6ff196b7e11c3
Ubuntu <ubuntu@ip-172-31-15-252.ec2.internal>
2022-02-25 13:59:18 +0000

    GVYAQuigfjkgvjklfdgjkregrhegjklfsnvbjksl

---
+++ basic_math.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Created By: Daniel
+# Date: Feb. 22, 2022
+# This program displays some math.
+
+
+def main():
+    print("25 / 3 = {}".format(25 / 3))
+    print("5^3 = {}".format(5**3))
+    print("37 + 24 = {}".format(37 + 24))
+    print("17 - 6 = {}".format(17 - 6))
+    print("5 x 13 = {}".format(5 * 13))
+    print("27 - (24 / 8)= {}".format(27 - (24 / 8)))
+    print("25 % 7 = {}".format(25 % 7))
+
+
+if __name__ == "__main__":
+    main()
